22407b8df11d6a20aeb1bb2ac38aa68d942186f4
SYT <18805571318syt@gmail.com>
2026-07-05 11:20:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/YutongSun-Personal-Website/",
+export default defineConfig(() => {
+  // 检查是否在 GitHub Actions 的环境中运行
+  const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
+  return {
+    plugins: [react()],
+    // 如果是 GitHub 部署，使用仓库名作为路径；如果是 Netlify/Vercel/本地开发，使用根路径 "/"
+    base: isGitHubActions ? "/YutongSun-Personal-Website/" : "/",
+  };
 });
